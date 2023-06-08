aee39bbbc39063ef088b157e0e8a06805ddc0f5a
Chinemerem Daniel <danpresh302@gmail.com>
2023-06-08 22:54:01 +0100

    modifying landing page

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -8,22 +8,40 @@ import {
   Text,
 } from "@chakra-ui/react";
 const Faqs = () => {
+  const accordionObj = {
+    flex: 1,
+    py: "1rem",
+    fontWeight: 700,
+    fontSize: "1.2rem",
+    textColor: "#747474",
+  };
+  const accordionPanel = {
+    pb: 4,
+    fontWeight: 300,
+    textColor: "#747474",
+  };
+
   return (
     <Box>
-      <Text as="h2" fontWeight={600} fontSize="2rem" py=".4rem">
+      <Text
+        as="h2"
+        fontWeight={600}
+        fontSize="2rem"
+        py=".4rem"
+        textAlign="center">
         Frequently asked questions(FAQs)
       </Text>
-      <Accordion>
+      <Accordion allowToggle bgColor="#FDECF3">
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+              <Box as="span" {...accordionObj} textAlign="left">
                 Is this a trend?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel pb={4}>
+          <AccordionPanel {...accordionPanel}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -34,13 +52,13 @@ const Faqs = () => {
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+              <Box as="span" {...accordionObj} textAlign="left">
                 Is this a trend?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel pb={4}>
+          <AccordionPanel {...accordionPanel}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -50,13 +68,13 @@ const Faqs = () => {
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+              <Box as="span" {...accordionObj} textAlign="left">
                 Is this a trend?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel pb={4}>
+          <AccordionPanel {...accordionPanel}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -66,13 +84,13 @@ const Faqs = () => {
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+              <Box as="span" {...accordionObj} textAlign="left">
                 Is this a trend?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel pb={4}>
+          <AccordionPanel {...accordionPanel}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -82,13 +100,13 @@ const Faqs = () => {
         <AccordionItem>
           <h2>
             <AccordionButton>
-              <Box as="span" flex="1" textAlign="left">
+              <Box as="span" {...accordionObj} textAlign="left">
                 Is this a trend?
               </Box>
               <AccordionIcon />
             </AccordionButton>
           </h2>
-          <AccordionPanel pb={4}>
+          <AccordionPanel {...accordionPanel}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
