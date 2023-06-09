24f8f81226dec62363659c05752d116e0d99ea52
Chinemerem Daniel <danpresh302@gmail.com>
2023-06-09 20:58:05 +0100

    building the testimonial section

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
+const Testimonials = () => {
+  return (
+    <Box
+      as="section"
+      py="3rem"
+      px="1rem"
+      bg="radial-gradient(circle, rgba(238,174,202,1) 14%, rgba(222,176,208,1) 34%, rgba(215,97,155,1) 94%)"></Box>
+  );
+};
+
+export default Testimonials;
