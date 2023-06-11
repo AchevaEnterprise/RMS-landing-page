b21842a555a90beebab529e7f2edfc7cfdaadba5
Chinemerem Daniel <danpresh302@gmail.com>
2023-06-11 23:33:34 +0100

    downloaded place holder images

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,18 +1,27 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
-import App from "../App";
+import image1 from "../Images/attractive.jpg";
+import image2 from "../Images/smile-1.jpg";
+import image3 from "../Images/smile-2.jpg";
 
 const Testimonials = () => {
   return (
     <Flex
       as="section"
-      h="60vh"
+      h="75vh"
       w="300vw"
       py="3rem"
-      px="1rem"
-      bg="radial-gradient(circle, rgba(238,174,202,1) 14%, rgba(222,176,208,1) 34%, rgba(215,97,155,1) 94%)">
-      <Flex>
+      px="2rem"
+      bg="radial-gradient(circle, rgba(238,174,202,1) 14%, rgba(222,176,208,1) 34%, rgba(215,97,155,1) 94%)"
+      bgRepeat="repeat-x">
+      <Flex h="80%" w="75vw" border="4px solid blue" m="auto 0">
         <Flex flexDir="column" gap=".6rem">
-          <Image />
+          <Image
+            src={image1}
+            alt="300 Level course Rep image"
+            w={200}
+            h="auto"
+            bgSize="cover"
+          />
           <Text></Text>
           <Text></Text>
         </Flex>
