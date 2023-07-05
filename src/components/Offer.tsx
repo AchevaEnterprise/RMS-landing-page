cc439adf56eb54f3e90078d8c24bb9d8a578d92d
Chinemerem Daniel <danpresh302@gmail.com>
2023-07-05 21:36:22 +0100

    finished building what we offer section

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 import alura from "../Images/alura.png";
 import aluraRemote from "../Images/aluraRemote.png";
 import creditCard from "../Images/creditCard.png";
@@ -21,15 +21,17 @@ const Offer = () => {
   }, []);
   return (
     <Box textColor="#492A34" my="2rem">
-      <Text
+      <Heading
         as="h1"
         textAlign="center"
         color="inherit"
         fontWeight={600}
         fontSize="2rem">
         What we offer
-      </Text>
+      </Heading>
+
       <Flex
+        gap="1rem"
         alignItems={{
           base: "center",
           lg: "space-between",
@@ -88,38 +90,43 @@ const Offer = () => {
         }}>
         <BiSolidChevronsDown size="5rem" color="#36819A" />
       </Flex>
+
       <Flex
+        gap="1rem"
         alignItems={{
           base: "center",
           lg: "space-between",
         }}
         justifyContent="space-between"
         flexDir={{
-          base: "column-reverse",
-          lg: "row",
-        }}
-        data-aos="zoom-in-down"
-        width={{
-          base: "70%",
-          lg: "50%",
+          base: "column",
+          lg: "row-reverse",
         }}>
-        <Text as="h2">Payment Options</Text>
-        <Text as="p">
-          Start your journey with RMS, open an account in your required section
-          and enjoy the journey.
-        </Text>
+        <Image
+          src={creditCard}
+          alt="credit-card"
+          data-aos="zoom-in-left"
+          width={{
+            base: "300px",
+            lg: "450px",
+          }}
+          h="auto"
+        />
 
-        <Flex>
-          <Image
-            src={creditCard}
-            alt="credit-card"
-            data-aos="zoom-in-left"
-            width={{
-              base: "300px",
-              lg: "450px",
-            }}
-            h="auto"
-          />
+        <Flex
+          flexDir="column"
+          width={{
+            base: "70%",
+            lg: "50%",
+          }}
+          data-aos="fade-left">
+          <Text as="h2" color="#36819A">
+            Payment Options:
+          </Text>
+          <Text as="p">
+            Start your journey with RMS, open an account in your required
+            section and enjoy the journey.
+          </Text>
         </Flex>
       </Flex>
       <Box
@@ -144,7 +151,9 @@ const Offer = () => {
         }}>
         <BiSolidChevronsDown size="5rem" color="#36819A" />
       </Flex>
+
       <Flex
+        gap="1rem"
         alignItems={{
           base: "center",
           lg: "space-between",
@@ -154,22 +163,29 @@ const Offer = () => {
           base: "column",
           lg: "row",
         }}>
-        <Flex>
-          <Image
-            src={happy}
-            alt="transcript-happy"
-            data-aos="fade-right"
-            width={{
-              base: "300px",
-              lg: "450px",
-            }}
-            h="auto"
-          />
-        </Flex>
-        <Flex flexDir="column" width="50%" data-aos="fade-left">
-          <Text as="h2">Transcript</Text>
+        <Image
+          src={happy}
+          alt="a happy person"
+          data-aos="fade-right"
+          width={{
+            base: "300px",
+            lg: "450px",
+          }}
+          h="auto"
+        />
+
+        <Flex
+          flexDir="column"
+          width={{
+            base: "70%",
+            lg: "50%",
+          }}
+          data-aos="fade-left">
+          <Text as="h2" color="#36819A">
+            Transcript:
+          </Text>
           <Text as="p">
-            Easily access your transcript and print out in pdf format.
+            Easily access your transcript and print out as pdf.
           </Text>
         </Flex>
       </Flex>
@@ -195,7 +211,9 @@ const Offer = () => {
         }}>
         <BiSolidChevronsDown size="5rem" color="#36819A" />
       </Flex>
+
       <Flex
+        gap="1rem"
         alignItems={{
           base: "center",
           lg: "space-between",
@@ -203,27 +221,33 @@ const Offer = () => {
         justifyContent="space-between"
         flexDir={{
           base: "column",
-          lg: "row",
-        }}
-        data-aos="fade-right">
-        <Flex>
-          <Text as="h2">Error Reduction</Text>
+          lg: "row-reverse",
+        }}>
+        <Image
+          src={aluraRemote}
+          alt="Renote-researcher"
+          data-aos="fade-left"
+          width={{
+            base: "300px",
+            lg: "450px",
+          }}
+          h="auto"
+        />
+
+        <Flex
+          flexDir="column"
+          width={{
+            base: "70%",
+            lg: "50%",
+          }}
+          data-aos="fade-left">
+          <Text as="h2" color="#36819A">
+            Error Reduction:
+          </Text>
           <Text as="p">
             Our system reduces errors to its barest minimum as grades are
             computed on the platform.
           </Text>
-        </Flex>
-        <Flex>
-          <Image
-            src={aluraRemote}
-            alt="remote-researcher"
-            data-aos="fade-left"
-            width={{
-              base: "300px",
-              lg: "450px",
-            }}
-            h="auto"
-          />
         </Flex>
       </Flex>
       <Box
@@ -248,7 +272,9 @@ const Offer = () => {
         }}>
         <BiSolidChevronsDown size="5rem" color="#36819A" />
       </Flex>
+
       <Flex
+        gap="1rem"
         alignItems={{
           base: "center",
           lg: "space-between",
@@ -258,20 +284,27 @@ const Offer = () => {
           base: "column",
           lg: "row",
         }}>
-        <Flex>
-          <Image
-            src={phone}
-            alt="phoneIcon"
-            data-aos="fade-right"
-            width={{
-              base: "300px",
-              lg: "450px",
-            }}
-            h="auto"
-          />
-        </Flex>
-        <Flex flexDir="column" width="50%" data-aos="fade-left">
-          <Text as="h2">Chatting features</Text>
+        <Image
+          src={phone}
+          alt="phone Icon"
+          data-aos="fade-right"
+          width={{
+            base: "300px",
+            lg: "450px",
+          }}
+          h="auto"
+        />
+
+        <Flex
+          flexDir="column"
+          width={{
+            base: "70%",
+            lg: "50%",
+          }}
+          data-aos="fade-left">
+          <Text as="h2" color="#36819A">
+            Chatting features:
+          </Text>
           <Text as="p">
             With our platform, contact has been made easier as the RMS seeks to
             reduce stress and encourage on-the-go activities.
